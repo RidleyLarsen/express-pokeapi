11c18ebadeab9cab00eebffe93d78a0b238ade97
Ridley Larsen <ridley@velocitywebworks.com>
2014-12-14 02:29:38 +0000

    Updated things; added modules; etc

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 var express = require('express');
+var jade = require('jade');
 var app = express();
 
 app.set('port', (process.env.PORT || 8000));
+app.set('view engine', 'jade')
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var Sequelize = require('sequelize')
+var sequelize = new Sequelize(
+  'rqypmrin_pokeapi',
+  'rqypmrin_ridley',
+  'stjggiLbada95IQCju8wU6hnB90fhekQ', {
+    dialect: "mysql", // or 'sqlite', 'postgres', 'mariadb'
+    port:    3306, // or 5432 (for postgres)
+  }
+)
+
+sequelize
+  .authenticate()
+  .complete(function(err) {
+    if (!!err) {
+      console.log('Unable to connect to the database:', err)
+    } else {
+      console.log('Connection has been established successfully.')
+    }
+  })
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: 'PokeAPI Explorer',
+    message: 'Hello there!'
+  });
 });
 
 app.listen(app.get('port'), function() {
